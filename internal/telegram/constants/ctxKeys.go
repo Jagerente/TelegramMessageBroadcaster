9cb7fcc9557281fa3b1b860babedaa0063f93764
Jagerente <JustlPains@gmail.com>
2023-04-20 21:05:58 +0700

    remove unused;

--- internal/telegram/constants/ctxKeys.go
+++ internal/telegram/constants/ctxKeys.go
@@ -5,9 +5,5 @@ const (
 	CtxArgs         string = "args"
 	CtxArgsRequired string = "args_required"
 	CtxController   string = "controller"
-
-	CtxChatArgsId      string = "chatid"
-	CtxChatArgsName    string = "chat_name"
-	CtxChatArgsLangId  string = "chat_langid"
-	CtxChatArgsGroupId string = "chat_groupid"
+	CtxUser         string = "user"
 )
