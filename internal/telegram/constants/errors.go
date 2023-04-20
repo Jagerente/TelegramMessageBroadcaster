e2d9bce8fc455d26ff50676a5149fd9d9549b69f
Jagerente <JustlPains@gmail.com>
2023-04-20 21:06:05 +0700

    more errors;

--- internal/telegram/constants/errors.go
+++ internal/telegram/constants/errors.go
@@ -3,3 +3,5 @@ package constants
 import "errors"
 
 var ErrInvalidInput = errors.New("invalid input")
+var ErrNotFound = errors.New("not found")
+var ErrAlreadyExists = errors.New("already exists")
